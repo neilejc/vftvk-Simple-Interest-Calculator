8c682cafb211868ed4f27a3a81c95237a5d0b539
Neil <62082307+neilejc@users.noreply.github.com>
2021-05-15 18:21:23 +1000

    updating js

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 
     //function to compute interest in calculator
-function compute(){
-    //establishing variables for compute 
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear() + parseInt(years);
-var compute = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+function compute()
 
-    //principal check if 0 or negative 
-if(principal < 1){
+{   //Establishing vars for computing
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var result= document.getElementById("result");
+    //Calcuation maths for interest
+
+    var interest=principal*years*rate/100; 
+    //Translates year selected to actual year
+
+    var actualYear=2021+parseFloat(years); 
+    //Testing for positive principal 
+
+    if(principal < 1){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
-}
-    //Display result on button click
-document.getElementById("result").innerHTML= compute;
-
+    }
+    //Calculation text to report with highlighting
+    else{ 
+        result.innerHTML=
+        `
+        If you deposit <mark>${principal}</mark>,<br>
+        at an interest rate of <mark>${rate}</mark>.<br>
+        You will receive an amount of <mark>${interest}</mark>,<br>
+        in the year <mark>${actualYear}</mark>
+        `
+    }
 }
     //function to update rate slider
 function updateRate() 
